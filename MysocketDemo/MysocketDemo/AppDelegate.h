58598e81e5fd9e21cc34ce20f0a574531b4ddbcf
jiangwei <439381577@qq.com>
2016-07-15 13:55:48 +0800

    初始化项目

---
+++ MysocketDemo/MysocketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MysocketDemo
+//
+//  Created by 姜孟东 on 16/7/15.
+//  Copyright © 2016年 jiangmengdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MysocketDemo/MysocketDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MysocketDemo
+//
+//  Created by 姜孟东 on 16/7/15.
+//  Copyright © 2016年 jiangmengdong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
